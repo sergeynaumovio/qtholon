1e833ad18244492c4dbe4e8bc68c76d84af2191f
Sergey Naumov <sergey@naumov.io>
2023-02-25 17:58:21 +0300

    ENH: widgets: stacked widget

--- src/widgets/holonstackedwidget.cpp
+++ src/widgets/holonstackedwidget.cpp
@@ -6,14 +6,29 @@
 
 class HolonStackedWidgetPrivate
 {
-    HolonStackedWidget *const q_ptr;
     const QString widgetGroup;
-    QHash<HolonAbstractTask *, QWidget *> taskWidget;
+
+protected:
+    HolonStackedWidget *const q_ptr;
 
 public:
     HolonStackedWidgetPrivate(HolonStackedWidget *q, const QString &group)
-    :   q_ptr(q),
-        widgetGroup(group)
+    :   widgetGroup(group),
+        q_ptr(q)
+    { }
+
+    virtual ~HolonStackedWidgetPrivate() { }
+
+    QString group() const { return widgetGroup; }
+};
+
+class HolonTaskStackedWidgetPrivate : public HolonStackedWidgetPrivate
+{
+    QHash<HolonAbstractTask *, QWidget *> taskWidget;
+
+public:
+    HolonTaskStackedWidgetPrivate(HolonTaskStackedWidget *q, const QString &group)
+    :   HolonStackedWidgetPrivate(q, group)
     { }
 
     void addTaskWidget(HolonAbstractTask *task, QWidget *widget)
@@ -22,37 +37,138 @@ public:
         q_ptr->addWidget(widget);
     }
 
-    QString group() const
-    {
-        return widgetGroup;
-    }
-
     void setCurrentTask(HolonAbstractTask *task)
     {
         if (taskWidget.contains(task))
             q_ptr->setCurrentWidget(taskWidget.value(task));
     }
+
+    QWidget *widget(HolonAbstractTask *task) const
+    {
+        return taskWidget.value(task, nullptr);
+    }
 };
 
-HolonStackedWidget::HolonStackedWidget(const QString &group)
+class HolonWindowStackedWidgetPrivate : public HolonStackedWidgetPrivate
+{
+    QHash<HolonAbstractWindow *, QWidget *> windowWidget;
+
+public:
+    HolonWindowStackedWidgetPrivate(HolonWindowStackedWidget *q, const QString &group)
+    :   HolonStackedWidgetPrivate(q, group)
+    { }
+
+    void addWindowWidget(HolonAbstractWindow *window, QWidget *widget)
+    {
+        windowWidget.insert(window, widget);
+        q_ptr->addWidget(widget);
+    }
+
+    void setCurrentWindow(HolonAbstractWindow *window)
+    {
+        if (windowWidget.contains(window))
+            q_ptr->setCurrentWidget(windowWidget.value(window));
+    }
+};
+
+class HolonWindowAreaStackedWidgetPrivate : public HolonStackedWidgetPrivate
+{
+    QHash<HolonWindowArea *, QWidget *> windowAreaWidget;
+
+public:
+    HolonWindowAreaStackedWidgetPrivate(HolonWindowAreaStackedWidget *q, const QString &group)
+    :   HolonStackedWidgetPrivate(q, group)
+    { }
+
+    void addWindowAreaWidget(HolonWindowArea *windowArea, QWidget *widget)
+    {
+        windowAreaWidget.insert(windowArea, widget);
+        q_ptr->addWidget(widget);
+    }
+
+    void setCurrentWindowArea(HolonWindowArea *windowArea)
+    {
+        if (windowAreaWidget.contains(windowArea))
+            q_ptr->setCurrentWidget(windowAreaWidget.value(windowArea));
+    }
+};
+
+
+HolonStackedWidget::HolonStackedWidget(HolonStackedWidgetPrivate &d)
 :   QStackedWidget(),
-    d_ptr(new HolonStackedWidgetPrivate(this, group))
+    d_ptr(&d)
 { }
 
 HolonStackedWidget::~HolonStackedWidget()
 { }
-
-void HolonStackedWidget::addTaskWidget(HolonAbstractTask *task, QWidget *widget)
-{
-    d_ptr->addTaskWidget(task, widget);
-}
 
 QString HolonStackedWidget::group() const
 {
     return d_ptr->group();
 }
 
-void HolonStackedWidget::setCurrentTask(HolonAbstractTask *task)
+
+HolonTaskStackedWidget::HolonTaskStackedWidget(const QString &group)
+:   HolonStackedWidget(*new HolonTaskStackedWidgetPrivate(this, group))
+{ }
+
+HolonTaskStackedWidget::~HolonTaskStackedWidget()
+{ }
+
+void HolonTaskStackedWidget::addTaskWidget(HolonAbstractTask *task, QWidget *widget)
 {
-    d_ptr->setCurrentTask(task);
+    Q_D(HolonTaskStackedWidget);
+    d->addTaskWidget(task, widget);
+}
+
+void HolonTaskStackedWidget::setCurrentTask(HolonAbstractTask *task)
+{
+    Q_D(HolonTaskStackedWidget);
+    d->setCurrentTask(task);
+}
+
+QWidget *HolonTaskStackedWidget::taskWidget(HolonAbstractTask *task) const
+{
+    Q_D(const HolonTaskStackedWidget);
+    return d->widget(task);
+}
+
+
+HolonWindowStackedWidget::HolonWindowStackedWidget(const QString &group)
+:   HolonStackedWidget(*new HolonWindowStackedWidgetPrivate(this, group))
+{ }
+
+HolonWindowStackedWidget::~HolonWindowStackedWidget()
+{ }
+
+void HolonWindowStackedWidget::addWindowWidget(HolonAbstractWindow *window, QWidget *widget)
+{
+    Q_D(HolonWindowStackedWidget);
+    d->addWindowWidget(window, widget);
+}
+
+void HolonWindowStackedWidget::setCurrentWindow(HolonAbstractWindow *window)
+{
+    Q_D(HolonWindowStackedWidget);
+    d->setCurrentWindow(window);
+}
+
+
+HolonWindowAreaStackedWidget::HolonWindowAreaStackedWidget(const QString &group)
+:   HolonStackedWidget(*new HolonWindowAreaStackedWidgetPrivate(this, group))
+{ }
+
+HolonWindowAreaStackedWidget::~HolonWindowAreaStackedWidget()
+{ }
+
+void HolonWindowAreaStackedWidget::addWindowAreaWidget(HolonWindowArea *windowArea, QWidget *widget)
+{
+    Q_D(HolonWindowAreaStackedWidget);
+    d->addWindowAreaWidget(windowArea, widget);
+}
+
+void HolonWindowAreaStackedWidget::setCurrentWindowArea(HolonWindowArea *windowArea)
+{
+    Q_D(HolonWindowAreaStackedWidget);
+    d->setCurrentWindowArea(windowArea);
 }
--- src/widgets/holonstackedwidget.h
+++ src/widgets/holonstackedwidget.h
@@ -7,22 +7,70 @@
 #include "qtholonglobal.h"
 #include <QStackedWidget>
 
-class HolonStackedWidgetPrivate;
 class HolonAbstractTask;
+class HolonAbstractWindow;
+class HolonStackedWidgetPrivate;
+class HolonTaskStackedWidgetPrivate;
+class HolonWindowArea;
+class HolonWindowAreaStackedWidgetPrivate;
+class HolonWindowStackedWidgetPrivate;
 
 class Q_HOLON_EXPORT HolonStackedWidget : public QStackedWidget
 {
     Q_OBJECT
 
+protected:
     const QScopedPointer<HolonStackedWidgetPrivate> d_ptr;
 
+    HolonStackedWidget(HolonStackedWidgetPrivate &d);
+
 public:
-    explicit HolonStackedWidget(const QString &group);
     ~HolonStackedWidget();
 
-    void addTaskWidget(HolonAbstractTask *task, QWidget *widget);
     QString group() const;
+};
+
+
+class Q_HOLON_EXPORT HolonTaskStackedWidget : public HolonStackedWidget
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(HolonTaskStackedWidget)
+
+public:
+    explicit HolonTaskStackedWidget(const QString &group = QString());
+    ~HolonTaskStackedWidget();
+
+    void addTaskWidget(HolonAbstractTask *task, QWidget *widget);
     void setCurrentTask(HolonAbstractTask *task);
+    QWidget *taskWidget(HolonAbstractTask *task) const;
+};
+
+
+class Q_HOLON_EXPORT HolonWindowStackedWidget : public HolonStackedWidget
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(HolonWindowStackedWidget)
+
+public:
+    explicit HolonWindowStackedWidget(const QString &group = QString());
+    ~HolonWindowStackedWidget();
+
+    void addWindowWidget(HolonAbstractWindow *window, QWidget *widget);
+    void setCurrentWindow(HolonAbstractWindow *window);
+};
+
+
+class Q_HOLON_EXPORT HolonWindowAreaStackedWidget : public HolonStackedWidget
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(HolonWindowAreaStackedWidget)
+
+public:
+    explicit HolonWindowAreaStackedWidget(const QString &group = QString());
+    ~HolonWindowAreaStackedWidget();
+
+    void addWindowAreaWidget(HolonWindowArea *windowArea, QWidget *widget);
+    void setCurrentWindowArea(HolonWindowArea *windowArea);
 };
 
 #endif // HOLONSTACKEDWIDGET_H
