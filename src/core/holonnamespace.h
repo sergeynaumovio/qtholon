2e4ed0f49a42c7b97c6c07e7ded2466b54748493
Sergey Naumov <sergey@naumov.io>
2023-03-17 19:14:37 +0300

    ENH: core: add window type enum to namespace

--- src/core/holonnamespace.h
+++ src/core/holonnamespace.h
@@ -12,6 +12,21 @@ struct QMetaObject;
 namespace Holon
 {
     Q_NAMESPACE_EXPORT(Q_HOLON_EXPORT)
+
+    enum WindowType
+    {
+        SidebarWindow = 0x0001,
+        TaskWindow = 0x0002,
+        WindowSplitButtonHint = 0x0008,
+        WindowMinMaxButtonsHint = 0x0010,
+        WindowCloseButtonHint = 0x00020,
+        WindowAllButtonsHint = WindowSplitButtonHint |
+                               WindowMinMaxButtonsHint |
+                               WindowCloseButtonHint
+    };
+    Q_DECLARE_FLAGS(WindowFlags, WindowType)
+    Q_DECLARE_OPERATORS_FOR_FLAGS(WindowFlags)
+    Q_FLAG_NS(WindowFlags)
 };
 
 #endif // HOLONNAMESPACE_H
