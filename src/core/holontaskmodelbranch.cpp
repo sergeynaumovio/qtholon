2794f95d904d815e8a198b7160df406693eb577e
Sergey Naumov <sergey@naumov.io>
2023-03-30 18:55:17 +0300

    ENH: core: task model branch expanded item

--- src/core/holontaskmodelbranch.cpp
+++ src/core/holontaskmodelbranch.cpp
@@ -2,24 +2,74 @@
 // SPDX-License-Identifier: 0BSD
 
 #include "holontaskmodelbranch.h"
+#include "holondesktop.h"
 #include "holontaskmodel.h"
+#include "holontheme.h"
+#include "holonthemeicons.h"
 #include <QIcon>
 
 using namespace Qt::Literals::StringLiterals;
 
+class HolonTaskModelBranchPrivate
+{
+    HolonTaskModel *const model;
+    HolonTaskModelBranch *const branch;
+
+public:
+    bool expanded{};
+
+    HolonTaskModelBranchPrivate(HolonTaskModel *m)
+    :   model(m),
+        branch(nullptr)
+    { }
+
+    HolonTaskModelBranchPrivate(HolonTaskModelBranch *b)
+    :   model(nullptr),
+        branch(b)
+    { }
+
+    HolonDesktop *desktop() const
+    {
+        return (model ? model->desktop() : branch->desktop());
+    }
+};
+
 HolonTaskModelBranch::HolonTaskModelBranch(QLoaderSettings *settings, HolonTaskModel *taskTreeModel)
 :   QObject(taskTreeModel),
-    QLoaderSettings(settings)
+    QLoaderSettings(settings),
+    d_ptr(new HolonTaskModelBranchPrivate(taskTreeModel))
 { }
 
 HolonTaskModelBranch::HolonTaskModelBranch(QLoaderSettings *settings, HolonTaskModelBranch *taskTreeBranch)
 :   QObject(taskTreeBranch),
-    QLoaderSettings(settings)
+    QLoaderSettings(settings),
+    d_ptr(new HolonTaskModelBranchPrivate(taskTreeBranch))
 { }
 
-QIcon HolonTaskModelBranch::icon() const
+HolonTaskModelBranch::~HolonTaskModelBranch()
+{ }
+
+HolonDesktop *HolonTaskModelBranch::desktop() const
 {
-    return QIcon(value(u"icon"_s, u":/holon/holoniconlight.svg"_s).toString());
+    return d_ptr->desktop();
+}
+
+QIcon HolonTaskModelBranch::icon(Holon::TreeItem state) const
+{
+    if (state == Holon::Collapsed)
+        return desktop()->currentTheme()->icons()->dirIcon();
+
+    return desktop()->currentTheme()->icons()->dirOpenIcon();
+}
+
+bool HolonTaskModelBranch::isExpanded() const
+{
+    return d_ptr->expanded;
+}
+
+void HolonTaskModelBranch::setExpanded(bool expanded)
+{
+    d_ptr->expanded = expanded;
 }
 
 QString HolonTaskModelBranch::title() const
--- src/core/holontaskmodelbranch.h
+++ src/core/holontaskmodelbranch.h
@@ -4,22 +4,31 @@
 #ifndef HOLONTASKMODELBRANCH_H
 #define HOLONTASKMODELBRANCH_H
 
+#include "holonnamespace.h"
 #include "qtholonglobal.h"
 #include <QLoaderSettings>
 #include <QObject>
 
 class HolonAbstractTask;
+class HolonDesktop;
 class HolonTaskModel;
+class HolonTaskModelBranchPrivate;
 
 class Q_HOLON_EXPORT HolonTaskModelBranch : public QObject, public QLoaderSettings
 {
     Q_OBJECT
 
+    const QScopedPointer<HolonTaskModelBranchPrivate> d_ptr;
+
 public:
     HolonTaskModelBranch(QLoaderSettings *settings, HolonTaskModel *taskTreeModel);
     HolonTaskModelBranch(QLoaderSettings *settings, HolonTaskModelBranch *taskTreeBranch);
+    ~HolonTaskModelBranch();
 
-    virtual QIcon icon() const;
+    HolonDesktop *desktop() const;
+    virtual QIcon icon(Holon::TreeItem expanded) const;
+    bool isExpanded() const;
+    void setExpanded(bool expanded);
     virtual QString title() const;
 };
 
