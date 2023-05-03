4218b1473205d786ffdbc4f200d2aa6cdf79cc80
Sergey Naumov <sergey@naumov.io>
2023-05-03 19:26:18 +0300

    ENH: core: workflow model branch

--- src/core/holonworkflowmodelbranch.cpp
+++ src/core/holonworkflowmodelbranch.cpp
@@ -2,19 +2,41 @@
 // SPDX-License-Identifier: 0BSD
 
 #include "holonworkflowmodelbranch.h"
+#include "holondesktop.h"
+#include "holontheme.h"
+#include "holonthemeicons.h"
+#include "holonworkflowitem.h"
 #include "holonworkflowmodel.h"
+#include "holonworkflowmodelbranch_p.h"
 #include <QIcon>
 
 using namespace Qt::Literals::StringLiterals;
 
 HolonWorkflowModelBranch::HolonWorkflowModelBranch(QLoaderSettings *settings, HolonWorkflowModel *workflowModel)
 :   QObject(workflowModel),
-    QLoaderSettings(this, settings)
+    QLoaderSettings(this, settings),
+    d_ptr(new HolonWorkflowModelBranchPrivate(this, workflowModel))
+{
+    workflowModel->addBranch(this);
+
+    if (!d_ptr->restoreTreeState())
+        emitWarning(u"task tree state format not valid"_s);
+}
+
+HolonWorkflowModelBranch::~HolonWorkflowModelBranch()
 { }
 
-QIcon HolonWorkflowModelBranch::icon() const
+void HolonWorkflowModelBranch::addTask(HolonAbstractTask *task)
 {
-    return QIcon(value(u"icon"_s, u":/holon/holoniconlight.svg"_s).toString());
+    d_ptr->addTask(task);
+}
+
+QIcon HolonWorkflowModelBranch::icon(Holon::TreeItem state) const
+{
+    if (state == Holon::Collapsed)
+        return workflowModel()->desktop()->currentTheme()->icons()->dirClosedIcon();
+
+    return workflowModel()->desktop()->currentTheme()->icons()->dirOpenIcon();
 }
 
 bool HolonWorkflowModelBranch::isCurrent() const
@@ -22,9 +44,19 @@ bool HolonWorkflowModelBranch::isCurrent() const
     return value(u"current"_s).toBool();
 }
 
+bool HolonWorkflowModelBranch::isExpanded() const
+{
+    return d_ptr->expanded;
+}
+
 void HolonWorkflowModelBranch::setCurrent(bool current)
 {
     setValue(u"current"_s, current);
+}
+
+void HolonWorkflowModelBranch::setExpanded(bool expanded)
+{
+    d_ptr->expanded = expanded;
 }
 
 QString HolonWorkflowModelBranch::title() const
@@ -34,5 +66,5 @@ QString HolonWorkflowModelBranch::title() const
 
 HolonWorkflowModel *HolonWorkflowModelBranch::workflowModel() const
 {
-    return qobject_cast<HolonWorkflowModel *>(parent());
+    return d_ptr->workflowModel;
 }
--- src/core/holonworkflowmodelbranch.h
+++ src/core/holonworkflowmodelbranch.h
@@ -4,22 +4,33 @@
 #ifndef HOLONWORKFLOWMODELBRANCH_H
 #define HOLONWORKFLOWMODELBRANCH_H
 
+#include "holonnamespace.h"
 #include "qtholonglobal.h"
 #include <QLoaderSettings>
 #include <QObject>
 
+class HolonAbstractTask;
 class HolonWorkflowModel;
+class HolonWorkflowModelBranchPrivate;
 
 class Q_HOLON_EXPORT HolonWorkflowModelBranch : public QObject, public QLoaderSettings
 {
     Q_OBJECT
 
+    friend class HolonWorkflowModelPrivate;
+    friend class HolonWorkflowModelBranchPrivate;
+    const QScopedPointer<HolonWorkflowModelBranchPrivate> d_ptr;
+
 public:
     explicit HolonWorkflowModelBranch(QLoaderSettings *settings, HolonWorkflowModel *workflowModel);
+    ~HolonWorkflowModelBranch();
 
-    virtual QIcon icon() const;
+    void addTask(HolonAbstractTask *task);
+    virtual QIcon icon(Holon::TreeItem expanded) const;
     bool isCurrent() const;
+    bool isExpanded() const;
     void setCurrent(bool current);
+    void setExpanded(bool expanded);
     virtual QString title() const;
     HolonWorkflowModel *workflowModel() const;
 };
