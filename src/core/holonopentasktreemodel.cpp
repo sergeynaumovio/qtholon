4a4c6db6963bee0c22d24fa5a96238ae2c12c80f
Sergey Naumov <sergey@naumov.io>
2024-11-25 10:10:17 +0300

    NEW: core: open task tree model

---
+++ src/core/holonopentasktreemodel.cpp
@@ -0,0 +1,112 @@
+// Copyright (C) 2024 Sergey Naumov <sergey@naumov.io>
+// SPDX-License-Identifier: 0BSD
+
+#include "holonopentasktreemodel.h"
+#include "holonabstracttask.h"
+#include "holoncore.h"
+#include "holondesktop.h"
+#include "holonworkflow.h"
+
+using namespace Qt::Literals::StringLiterals;
+
+class HolonOpenTaskTreeModelPrivate
+{
+public:
+    HolonOpenTaskTreeModel *const q_ptr;
+    QList<HolonAbstractTask *> openTaskList;
+    QModelIndex currentIndex;
+
+    HolonOpenTaskTreeModelPrivate(HolonOpenTaskTreeModel *q)
+    :   q_ptr(q)
+    { }
+
+    int rowCount()
+    {
+        if (openTaskList.isEmpty())
+            for (HolonWorkflow *workflow : q_ptr->QObject::parent()->findChildren<HolonWorkflow *>())
+                for (HolonAbstractTask *task : workflow->findChildren<HolonAbstractTask *>())
+                    if (task->isOpen())
+                        openTaskList.append(task);
+
+        return openTaskList.size();
+    }
+};
+
+HolonOpenTaskTreeModel::HolonOpenTaskTreeModel(HolonCore *core)
+:   QAbstractItemModel(core),
+    d_ptr(this)
+{ }
+
+HolonOpenTaskTreeModel::HolonOpenTaskTreeModel(HolonDesktop *desktop)
+:   QAbstractItemModel(desktop),
+    d_ptr(this)
+{ }
+
+HolonOpenTaskTreeModel::~HolonOpenTaskTreeModel()
+{ }
+
+int HolonOpenTaskTreeModel::columnCount(const QModelIndex &) const
+{
+    return 2;
+}
+
+QVariant HolonOpenTaskTreeModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid() || index.column())
+        return QVariant();
+
+    if (index.row() < d_ptr->openTaskList.size())
+    {
+        HolonAbstractTask *task = d_ptr->openTaskList.at(index.row());
+
+        if (role == Qt::DecorationRole)
+            return task->icon();
+
+        if (role == Qt::DisplayRole)
+            return task->title();
+    }
+
+    return QVariant();
+}
+
+QModelIndex HolonOpenTaskTreeModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return QModelIndex();
+
+    QModelIndex index = createIndex(row, column, d_ptr->openTaskList.at(row));
+
+    HolonAbstractTask *task = d_ptr->openTaskList.at(row);
+    if (task->isCurrent())
+        d_ptr->currentIndex = index;
+
+    return index;
+}
+
+bool HolonOpenTaskTreeModel::insertRows(int position, int rows, const QModelIndex &parent)
+{
+    beginInsertRows(parent, position, position + rows);
+    if (HolonDesktop *desktop = qobject_cast<HolonDesktop *>(QObject::parent()))
+        d_ptr->openTaskList.append(desktop->workflow()->findChildren<HolonAbstractTask *>().constLast());
+    endInsertRows();
+
+    return true;
+}
+
+QModelIndex HolonOpenTaskTreeModel::parent(const QModelIndex &) const
+{
+    return QModelIndex();
+}
+
+QModelIndex HolonOpenTaskTreeModel::restoreCurrentIndex() const
+{
+    return d_ptr->currentIndex;
+}
+
+int HolonOpenTaskTreeModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+
+    return d_ptr->rowCount();
+}
---
+++ src/core/holonopentasktreemodel.h
@@ -0,0 +1,35 @@
+// Copyright (C) 2024 Sergey Naumov <sergey@naumov.io>
+// SPDX-License-Identifier: 0BSD
+
+#ifndef HOLONOPENTASKTREEMODEL_H
+#define HOLONOPENTASKTREEMODEL_H
+
+#include <QAbstractItemModel>
+#include <QScopedStorage>
+
+class HolonCore;
+class HolonDesktop;
+class HolonOpenTaskTreeModelPrivate;
+
+class HolonOpenTaskTreeModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+    QScopedStorage<HolonOpenTaskTreeModelPrivate, 56> d_ptr;
+
+public:
+    explicit HolonOpenTaskTreeModel(HolonCore *core);
+    explicit HolonOpenTaskTreeModel(HolonDesktop *desktop);
+
+    ~HolonOpenTaskTreeModel();
+
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QModelIndex index(int row, int column = 0, const QModelIndex &parent = QModelIndex()) const override;
+    bool insertRows(int position, int rows, const QModelIndex &parent = QModelIndex()) override;
+    QModelIndex parent(const QModelIndex &child) const override;
+    QModelIndex restoreCurrentIndex() const;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+};
+
+#endif // HOLONOPENTASKTREEMODEL_H
