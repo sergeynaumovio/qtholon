94543516e3c669dc8f2fcd2b8a8630428cbf0739
Sergey Naumov <sergey@naumov.io>
2024-12-03 11:49:32 +0300

    FIX: core: next child id

--- src/core/holonid.cpp
+++ src/core/holonid.cpp
@@ -1,13 +1,15 @@
-// Copyright (C) 2023 Sergey Naumov <sergey@naumov.io>
+// Copyright (C) 2024 Sergey Naumov <sergey@naumov.io>
 // SPDX-License-Identifier: 0BSD
 
 #include "holonid.h"
 
 uint HolonId::createChildId(QObject *parent)
 {
+    uint index;
     QSet<uint> children;
     for (QObject *o : parent->children())
-        children.insert(o->objectName().toUInt());
+        if (bool ok = (index = o->objectName().toUInt(&ok), ok))
+            children.insert(index);
 
     int id{-1};
     while (children.contains(++id));
