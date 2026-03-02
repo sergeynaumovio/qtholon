d84d5b266c9ea6757e2ec5c80da369553e713e5a
Sergey Naumov <sergey@naumov.io>
2026-03-02 22:37:40 +0300

    ENH: widgets: remove "current" property if not used

--- src/widgets/holonabstractwindow_p.cpp
+++ src/widgets/holonabstractwindow_p.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 Sergey Naumov <sergey@naumov.io>
+// Copyright (C) 2026 Sergey Naumov <sergey@naumov.io>
 // SPDX-License-Identifier: 0BSD
 
 #include "holonabstractwindow_p.h"
@@ -16,5 +16,5 @@ HolonAbstractWindowPrivate::~HolonAbstractWindowPrivate()
 
 void HolonAbstractWindowPrivate::setCurrent(bool current)
 {
-    q_ptr->setValue(u"current"_s, current);
+    q_ptr->setValue(u"current"_s, (current ? true : QVariant()));
 }
